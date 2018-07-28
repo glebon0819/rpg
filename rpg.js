633fbac8db71d4839aa34f0694ed3e7e3775bcbe
glebon0819 <g.lebon0819@gmail.com>
2018-07-27 21:56:50 -0500

    Added mapping of commands to functions
    Added routing of commands to functions and began work on data storage and retrieval.

--- rpg.js
+++ rpg.js
@@ -1,26 +1,39 @@
 const readline = require('readline');
 const fs = require('fs');
+const test = require('./test');
 
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
 
+// keeps track of number of commands in a game
 var cycle = 0;
 
 function run(){
-	if(cycle < 1){
-		console.log('\n!=============== [Welcome to Test RPG!] ===============!\n\nEnter a command to continue...\n\n!======================================================!');
+	
+	if(cycle === 0){
+		console.log('\n !=============== [Welcome to Test RPG!] ===============!\n\n   % Enter a command to continue...\n');
 	}
-	rl.question('\n', (answer1) => {
-		console.log('You said: "' + answer1 + '"');
 
+	rl.question(' ', (command) => {
+
+		var commands = command.split(' ');
+
+		var root_command = commands[0];
 		
-		
-		if(answer1 == 'stop'){
+		if(root_command == 'exit'){
 			rl.close();
-			console.log('rl closed');
+			console.log('\n !======================================================!');
 			return;
+		}
+		else{
+			try {
+				test.commandMap[root_command]();
+			}
+			catch(err){
+				console.log(`\n   % '${root_command}' is not recognized as a command. For a list of valid commands, type 'commands'.\n`);
+			}
 		}
 
 		cycle++;

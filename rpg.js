e6210d2e69a4e772cc4a044ecbcc2147d8e023ea
glebon0819 <g.lebon0819@gmail.com>
2018-07-31 07:09:01 -0400

    Created basic HP and AP system and consumption and stat functionality
    Players can now assign unassigned stat points to stats, view their stats, view their HP, and eat food and drink beverages to regain lost HP or AP. Also added a minor change to the message that displays on startup.

--- rpg.js
+++ rpg.js
@@ -87,7 +87,7 @@ function getSaves(){
 }
 
 // function that adds an item to player inventory
-function addToInv(itm, num, qty){
+function addToInv(itm, num, qty, typ){
 	// get inventory items
 	var inv = Object.keys(userData.inv);
 	var match = false;
@@ -101,7 +101,7 @@ function addToInv(itm, num, qty){
 	if(match){
 		userData.inv[itm].qty = userData.inv[itm].qty + qty;
 	}else{
-		userData.inv[itm] = {og : num, qty : qty};
+		userData.inv[itm] = {typ: typ, og : num, qty : qty};
 	}
 
 	// print to screen message notifying player that an item was added to their inventory
@@ -195,7 +195,6 @@ function checkCooldown(cmd){
 // cleans up cooldowns, buffs, etc. that are left over from the last session
 // removes expired things and sets timeouts for things that are still active
 function cleanUp(){
-	console.log('cleanup begun');
 	for(var command in userData.cooldowns){
 		var timeleft = userData.cooldowns[command].dur - Math.floor((getTimestamp(false) - userData.cooldowns[command].beg) / 1000);
 		if(timeleft > 0){
@@ -205,6 +204,52 @@ function cleanUp(){
 			delete userData.cooldowns[command];
 		}
 	}
+}
+
+// returns whether a string is a stat
+function isStat(str){
+	var isStat = false;
+	Object.keys(userData.stats).forEach(stat => {
+		if(stat == str){
+			isStat = true;
+		}
+	});
+	return isStat;
+}
+
+// returns whether an item is in the player's inventory
+function isInInv(itm){
+	var isInInv = false;
+	Object.keys(userData.inv).forEach(item => {
+		if(item == itm){
+			isInInv = true;
+		}
+	});
+	return isInInv;
+}
+
+// removes item from inventory and applies its buffs and effects
+function consume(itm){
+	// get id of item from inventory and use it to access item's description in encyclopedia
+	var id = userData.inv[itm].og;
+
+	var effects = items[id].effects;
+	for(var effect in effects){
+		if(effect == 'hp'){
+			userData.gen.hp += effects[effect];
+			if(userData.gen.hp > userData.gen.hpm){
+				userData.gen.hp = userData.gen.hpm;
+			}
+		}
+		else if(effect == 'ap'){
+			userData.gen.ap += effects[effect];
+			if(userData.gen.ap > userData.gen.apm){
+				userData.gen.ap = userData.gen.apm;
+			}
+		}
+	}
+
+	remFromInv(itm, 1);
 }
 
 // maps commands to functions and includes data about commands
@@ -240,6 +285,10 @@ var commandMap = {
 							dat : getTimestamp(true),
 							dt2 : getTimestamp(false),
 							lvl : 1,
+							hp : 100,
+							hpm : 100,
+							ap : 100,
+							apm : 100,
 							exp : 0,
 							gld : 5,
 							slv : 50,
@@ -249,14 +298,23 @@ var commandMap = {
 						},
 						inv: {
 							'Health Potion' : {
+								typ : 'potion',
 								og : 1,
+								qty : 1
+							},
+							'Poison' : {
+								typ : 'beverage',
+								og : 5,
 								qty : 1
 							}
 						},
 						stats: {
-							str : 0,
-							int : 0,
-							lck : 0
+							strength : 0,
+							stamina : 0,
+							resilience : 0,
+							intellect : 0,
+							luck : 0,
+							agility : 0
 						},
 						cooldowns: {}
 					};
@@ -375,15 +433,27 @@ var commandMap = {
 		}
 	},
 	// shows the user a list of the items in their inventory
+	'inv' : {
+		'grp' : 'Inventory',
+		'func' : function(cmd){
+			if(Object.keys(userData.inv).length > 0){
+				console.log(`\n ========================================================\n   ${userData.gen.nam}'s Inventory\n --------------------------------------------------------\n`)
+				var items = Object.keys(userData.inv);
+				items.forEach(item => {
+					console.log('   ' + userData.inv[item].qty + ` x ${item}\n`);
+				});
+				console.log(' ========================================================\n');
+			}
+			else{
+				console.log('\n   Your inventory is empty.\n');
+			}
+		}
+	},
+	// shows the user a list of the items in their inventory
 	'inventory' : {
 		'grp' : 'Inventory',
 		'func' : function(cmd){
-			console.log(`\n ========================================================\n   ${userData.gen.nam}'s Inventory\n --------------------------------------------------------\n`)
-			var items = Object.keys(userData.inv);
-			items.forEach(item => {
-				console.log('   ' + userData.inv[item].qty + ` x ${item}\n`);
-			});
-			console.log(' ========================================================\n');
+			commandMap['inv'].func(cmd);
 		}
 	},
 	// drops an item from inventory
@@ -409,12 +479,47 @@ var commandMap = {
 	'stats' : {
 		'grp' : 'Stats',
 		'func' : function(cmd){
-
+			var stats = Object.keys(userData.stats);
+			console.log(`\n   ${userData.gen.nam}'s Stats:\n`);
+			stats.forEach(stat => {
+				console.log(`   + ${stat}: ${userData.stats[stat]}`);
+			});
+			console.log();
 		}
 	},
 	// used to assign points to different stats
 	'assign' : {
 		'grp' : 'Stats',
+		'func' : function(cmd){
+			cmd.shift();
+			if(Object.keys(cmd).length === 2){
+				if(isStat(cmd[Object.keys(cmd)[0]])){
+					if(parseInt(cmd[Object.keys(cmd)[1]]) > 0){
+						if(userData.gen.poi >= parseInt(cmd[Object.keys(cmd)[1]])){
+							userData.stats[cmd[Object.keys(cmd)[0]]] += parseInt(cmd[Object.keys(cmd)[1]]);
+							userData.gen.poi -= parseInt(cmd[Object.keys(cmd)[1]]);
+							console.log(`\n   '${cmd[Object.keys(cmd)[0]]}' changed to ${userData.stats[cmd[Object.keys(cmd)[0]]]}.\n`);
+						}
+						else{
+							console.log('\n   Insufficient unassigned stat points.\n');
+						}
+					}
+					else{
+						console.log('\n   Use a number.\n');
+					}
+				}
+				else{
+					console.log('\n   That is not a stat.\n');
+				}
+			}
+			else{
+				console.log('\n   Arguments improperly given. Give a stat followed by the number of points you would like to assign to that stat.\n');
+			}
+		}
+	},
+	// shows the user general information about themself
+	'me' : {
+		'grp' : 'Miscellaneous',
 		'func' : function(cmd){
 
 		}
@@ -423,14 +528,62 @@ var commandMap = {
 	'drink' : {
 		'grp' : 'Inventory',
 		'func' : function(cmd){
-			console.log('\n   You drink a potion. HP: (100/100)\n');
+			cmd.shift();
+			var itm = cmd.join(' ');
+
+			// check if item is defined
+			if(itm.length > 0){
+				// check if is in inventory
+				if(isInInv(itm)){
+					// check if is food
+					if(userData.inv[itm].typ == 'potion' || userData.inv[itm].typ == 'beverage'){
+						// consume (which removes it from inventory and applies the buff/effect)
+						consume(itm);
+						console.log(`\n   You drink ${itm}.\n`);
+					}
+					else{
+						console.log('\n   You cannot drink that item.\n');
+					}
+				}
+				else{
+					console.log(`\n   '${itm}' not found in your inventory.\n`);
+				}
+				//console.log('\n   You eat an apple. HP: (100/100)\n');
+			}
+			else{
+				console.log('\n   No item provided to drink.\n');
+			}
 		}
 	},
 	// can be used to consume food items
 	'eat' : {
 		'grp' : 'Inventory',
 		'func' : function(cmd){
-			console.log('\n   You eat an apple. HP: (100/100)\n');
+			cmd.shift();
+			var itm = cmd.join(' ');
+
+			// check if item is defined
+			if(itm.length > 0){
+				// check if is in inventory
+				if(isInInv(itm)){
+					// check if is food
+					if(userData.inv[itm].typ == 'food'){
+						// consume (which removes it from inventory and applies the buff/effect)
+						consume(itm);
+						console.log(`\n   You ate ${itm}.\n`);
+					}
+					else{
+						console.log('\n   You cannot eat that item.\n');
+					}
+				}
+				else{
+					console.log(`\n   '${itm}' not found in your inventory.\n`);
+				}
+				//console.log('\n   You eat an apple. HP: (100/100)\n');
+			}
+			else{
+				console.log('\n   No item provided to eat.\n');
+			}
 		}
 	},
 	// continues your latest battle
@@ -440,40 +593,44 @@ var commandMap = {
 			console.log('\n   You\'re now on an adventure.\n');
 		}
 	},
+	// forages in the local area, adding berries to your inventory
+	'forage' : {
+		'grp' : 'Gathering',
+		'func' : function(cmd){
+
+			addToInv('Berries', 6, 2, 'food');
+
+			// create cooldown
+			createCooldown('forage', 20);
+		}
+	},
 	// fishes in the local area, adding fish to your inventory
 	'fish' : {
 		'grp' : 'Gathering',
 		'func' : function(cmd){
-			// checks if any fishing cooldowns are in effect
-			// if so, print that it is not ready yet with the time left
-			// else add a fish to the player's inventory
-			addToInv('Fish', 3, 1);
+
+			addToInv('Fish', 3, 1, 'food');
 
 			// create cooldown
 			createCooldown('fish', 30);
-
-			// or maybe always check all functions for cooldowns before running them
 		}
 	},
-	// chops down in the local area, adding fish to your inventory
+	// chops down in the local area, adding logs to your inventory
 	'chop' : {
 		'grp' : 'Gathering',
 		'func' : function(cmd){
-			// checks if any chopping cooldowns are in effect
-			// if so, print that it is not ready yet with the time left
-			// else add a log to the player's inventory
+
 			addToInv('Log', 2, 1);
 
 			// create cooldown
 			createCooldown('chop', 30);
 		}
 	},
+	// mines down in the local area, adding iron ore to your inventory
 	'mine' : {
 		'grp' : 'Gathering',
 		'func' : function(cmd){
-			// checks if any mining cooldowns are in effect
-			// if so, print that it is not ready yet with the time left
-			// else add ore to the player's inventory
+
 			addToInv('Iron Ore', 2, 1);
 
 			// create cooldown
@@ -507,17 +664,31 @@ var commandMap = {
 			console.log('\n !======================================================!');
 			process.exit(0);
 		}
+	},
+
+	'health' : {
+		'func' : function(cmd){
+			console.log(`\n   Your HP: (${userData.gen.hp}/${userData.gen.hpm}).\n`);
+		}
+	},
+
+	'hp' : {
+		'func' : function(cmd){
+			commandMap['health'].func(cmd);
+		}
 	}
 };	
 
 function run(){
 
+	// code that runs on startup
 	if(sessionData.cycle === 0){
 		loadResources();
 
 		console.log('\n !=============== [Welcome to Test RPG!] ===============!');
 		commandMap['profiles'].func('');
-		console.log('   Use \'new\' followed by your desired username to start\n   a new game or \'load\' followed by the username of your\n   desired profile to load a previously saved game.\n');
+		console.log('   Use the \'new\' command followed by your desired\n   username to start a new game or \'load\' followed by the\n   username of your desired profile to load a previously\n   saved game.\n');
+		console.log('   For help, use \'commands\' to display a list of\n   commands that can be used in the game.\n');
 	}
 
 	rl.question(' ', (command) => {
